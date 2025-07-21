924d2ba3fc3ea46e1758f7b6a6c966d55c2b2bc3
Gabriel Martín Moran <gabriel.moran@fluxit.com.ar>
2025-07-21 16:23:35 -0300

    Removed unnecesary import

--- src/commands/pytests-results-explorer/screen-handler.ts
+++ src/commands/pytests-results-explorer/screen-handler.ts
@@ -14,7 +14,6 @@ import path from 'path';
 
 import { $ } from 'bun';
 import { extractDiffStrings, type Diff } from './diff-extractor';
-import { left, right } from 'inquirer/lib/utils/readline';
 
 interface TestList extends Widgets.ListElement {
     selected: number;
